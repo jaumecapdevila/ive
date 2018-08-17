4e8ec9f2ad2e45ee9f0d7ffcccb1c63cfb8f435f
jaumecapdevila <contact@jaumecapdevila.net>
2018-08-17 10:24:46 +0100

    Implement basic options page

---
+++ dist/options/options.js
@@ -0,0 +1,21 @@
+function saveOptions(event) {
+  event.preventDefault();
+  browser.storage.local.set({
+    bar: document.getElementById('status-bar').checked,
+  });
+}
+
+function restoreOptions() {
+  var getting = browser.storage.local.get('bar');
+  getting.then(
+    function(result) {
+      document.getElementById('status-bar').checked = result.bar;
+    },
+    function(error) {
+      // TODO notificate the error
+    }
+  );
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('settings').addEventListener('submit', saveOptions);
